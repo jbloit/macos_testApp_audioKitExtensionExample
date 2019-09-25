81ffd82ceb9434226e270967e6981cdcca35cd40
Julien Bloit <julien.bloit@gmail.com>
2019-09-25 10:00:56 +0200

    Initial Commit

---
+++ akcoreTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  akcoreTest
+//
+//  Created by julien@macmini on 25/09/2019.
+//  Copyright © 2019 jbloit. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
